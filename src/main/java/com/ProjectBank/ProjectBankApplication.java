8a7334640844c6371f431dd5395fdddba2a63882
kushal <kushalshrestha0808@gmail.com>
2023-03-01 14:54:03 -0500

    First commit

---
+++ src/main/java/com/ProjectBank/ProjectBankApplication.java
@@ -0,0 +1,13 @@
+package com.ProjectBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ProjectBank/ProjectBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjectBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
